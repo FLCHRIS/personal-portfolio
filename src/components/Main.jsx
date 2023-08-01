97220604a8e5f2597b4106e2873e1cb8e66bb29f
Christian <filetelopezchristian@gmail.com>
2023-08-01 14:47:28 -0500

    Archivo terminado :smiling_face_with_three_hearts:

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { About } from './About'
 import { Do } from './Do'
+import { Projects } from './Projects'
 
 export const Main = () => {
   return (
     <main className='main'>
       <About />
       <Do />
+      <Projects />
     </main>
   )
 }
