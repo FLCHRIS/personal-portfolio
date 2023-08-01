05d38a742cc9dc837500c93ab249aefb9e2dbfa1
Christian <filetelopezchristian@gmail.com>
2023-08-01 10:42:54 -0500

    Se agregó el componente Do

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { About } from './About'
+import { Do } from './Do'
 
 export const Main = () => {
   return (
     <main className='main'>
       <About />
+      <Do />
     </main>
   )
 }
