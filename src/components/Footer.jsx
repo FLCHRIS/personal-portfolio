80385d0d592dff17a61c3d961bb2f87eb7ffc52f
Christian <filetelopezchristian@gmail.com>
2023-08-01 11:43:37 -0500

    Se agregaron animaciones :star_struck:

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,44 @@
 import React from 'react'
 import { AiFillHeart } from 'react-icons/ai'
+import { motion } from 'framer-motion'
 
 export const Footer = () => {
   return (
     <footer className='footer'>
-      <p className='footer__copy'>
+      <motion.p
+        initial={{
+          translateY: 50,
+          opacity: 0
+        }}
+        whileInView={{
+          translateY: 0,
+          opacity: 1
+        }}
+        transition={{
+          duration: 1
+        }}
+        className='footer__copy'
+      >
         Copyright 2023 Christian FL
-      </p>
-      <p className='footer__build'>
+      </motion.p>
+      <motion.p
+        initial={{
+          translateY: 50,
+          opacity: 0
+        }}
+        whileInView={{
+          translateY: 0,
+          opacity: 1
+        }}
+        transition={{
+          delay: 0.5,
+          duration: 1
+        }}
+        className='footer__build'
+      >
         Development with <AiFillHeart aria-hidden='true' className='footer__icon' /> by
         <a href='https://github.com/FLCHRIS' target='_blank' rel='noreferrer'> flchris</a>
-      </p>
+      </motion.p>
     </footer>
   )
 }
